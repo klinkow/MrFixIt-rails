60b6038c09f4410ec87226d11894f25559d02806
Bill Klinkow <klinkow@gmail.com>
2017-02-24 12:50:53 -0800

    redo allowing workers to cliam a job via AJAX with a form.

---
+++ app/assets/javascripts/jobs.js
@@ -0,0 +1,7 @@
+$(document).ready() {
+
+  $('.edit_form input[type=checkbox]').click(fucntion() {
+
+  };)
+
+}
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -25,15 +25,15 @@ class JobsController < ApplicationController
   def update
     @job = Job.find(params[:id])
     if current_worker
-      if @job.update(pending: true, worker_id: current_worker.id)
-        respond_to do |format|
-          format.html { redirect_to worker_path(current_worker), notice: "You've successfully claimed this job." }
-          format.js
+        if @job.update(job_params)
+          respond_to do |format|
+            format.html { redirect_to worker_path(current_worker), notice: "You've successfully claimed this job." }
+            format.js
+          end
+        else
+          render :show
+          flash[:notice] = "Something went wrong!"
         end
-      else
-        render :show
-        flash[:notice] = "Something went wrong!"
-      end
     else
       # We need to streamline this process better in the future! - Mr. Fix-It.
       flash[:notice] = 'You must have a worker account to claim a job. Register for one using the link in the navbar above.'
@@ -44,7 +44,7 @@ class JobsController < ApplicationController
 private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :pending, :worker_id, :completed)
   end
 
 end
